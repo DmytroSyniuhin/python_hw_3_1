e029e168b382593fead4d8d0c151e62255240f9b
DmytroSyniuhin <dmytrosyniuhin@gmail.com>
2024-01-25 02:10:35 +0200

    calculator

--- hw_3_1.py
+++ hw_3_1.py
@@ -5,12 +5,12 @@ number_2 = input("Введіть друге число: ")
 if mathematical_operation == "/" and number_2 == "0":
     print("Ділення на нуль неможливе!")
 elif mathematical_operation == "+":
-    print(int(number_1) + int(number_2))
+    print(float(number_1) + float(number_2))
 elif mathematical_operation == "-":
-    print(int(number_1) - int(number_2))
+    print(float(number_1) - float(number_2))
 elif mathematical_operation == "*":
-    print(int(number_1) * int(number_2))
+    print(float(number_1) * float(number_2))
 elif mathematical_operation == "/":
-    print(int(number_1) / int(number_2))
+    print(float(number_1) / float(number_2))
 else:
     print("Перевірте чи вірно Ви ввели дані")
