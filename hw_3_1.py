4a04bf073ccda26f78754236d1413bb06ca959f1
DmytroSyniuhin <dmytrosyniuhin@gmail.com>
2024-01-25 02:24:28 +0200

    calculator

--- hw_3_1.py
+++ hw_3_1.py
@@ -1,14 +1,16 @@
-number_1 = input("Введіть перше число: ")
+number_1 = float(input("Введіть перше число: "))
 mathematical_operation = input("Введіть математичну дію (+, -, *, /): ")
-number_2 = input("Введіть друге число: ")
+number_2 = float(input("Введіть друге число: "))
 
-if mathematical_operation == "/" and number_2 == "0":
+if mathematical_operation == "/" and number_2 == 0:
     print("Ділення на нуль неможливе!")
 elif mathematical_operation == "+":
-    print(float(number_1) + float(number_2))
+    print(number_1 + number_2)
 elif mathematical_operation == "-":
-    print(float(number_1) - float(number_2))
+    print(number_1 - number_2)
 elif mathematical_operation == "*":
-    print(float(number_1) * float(number_2))
+    print(number_1 * number_2)
 elif mathematical_operation == "/":
-    print(float(number_1) / float(number_2))
+    print(number_1 / number_2)
+else:
+    print("Перевірте чи вірно Ви ввели дані")
